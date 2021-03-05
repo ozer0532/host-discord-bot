5000d8c49b392eba803e2f678cd518e2fb59fd74
Chokyi Ozer <13518107@std.stei.itb.ac.id>
2021-03-05 11:35:11 +0700

    Fixed config.js to not cause crash when there is no config.json file

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+
+if (!fs.existsSync('./config.json')) {
+    fs.writeFileSync('./config.json', JSON.stringify({}))
+}
+
 const config = require('./config.json');
 
 module.exports = function(name) {
